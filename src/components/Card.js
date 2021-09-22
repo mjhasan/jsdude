cfe2079be933f7a2d12c20e5a3a76ed04fe5dbc3
mjhasan <info.mjhasan@gmail.com>
2021-09-22 22:02:22 +0600

    database added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,17 @@
-import image from "../assets/images/3.jpg";
 import style from "../styles/Card.module.css";
 
- const Card = () => {
+const Card = ({ video }) => {
+  const { noq, title, youtubeID } = video;
   return (
-      <div className={style.card}>
-        <img src={image} alt="Card Title" />
-        <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-        <div className={style.qmeta}>
-          <p>10 Questions</p>
-          <p>Score : Not taken yet</p>
-        </div>
+    <div className={style.card}>
+      <img src={`http://img.youtube.com/vi/${youtubeID}/maxresdefault.jpg`}
+        alt={title} />
+      <p>{title}</p>
+      <div className={style.qmeta}>
+        <p>{noq} Questions</p>
+        <p>Total Point : {noq * 2}</p>
       </div>
+    </div>
   );
 }
 export default Card;
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,26 +1,33 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import useVideos from "../hooks/useVideos";
 import style from "../styles/Cards.module.css";
 import Card from "./Card";
 
 const Cards = () => {
+  const [page, setPage] = useState(1);
+  const { loading, error, videos, hasMore } = useVideos(page);
+
   return (
-    <div className={style.cards}>
-      <Link to="/quiz">
-        <Card />
-      </Link>
-      <Link to="/quiz">
-        <Card />
-      </Link>
-      <Link to="/quiz">
-        <Card />
-      </Link>
-      <Link to="/quiz">
-        <Card />
-      </Link>
-      <Link to="/quiz">
-        <Card />
-      </Link>
-    </div>
+    <>
+      {videos.length > 0 && (
+        <InfiniteScroll
+          dataLength={videos.length}
+          hasMore={hasMore}
+          loader="Loading..."
+          next={() => setPage(page + 8)}
+          className={style.cards}
+        >
+          {
+            videos.map((video) => <Card video={video} />)
+          }
+        </InfiniteScroll>
+      )}
+
+      {!loading && videos.length === 0 && <div>No data found!</div>}
+      {error && <div>There was an error!</div>}
+      {loading && <div>Loading...</div>}
+    </>
   );
 }
 
